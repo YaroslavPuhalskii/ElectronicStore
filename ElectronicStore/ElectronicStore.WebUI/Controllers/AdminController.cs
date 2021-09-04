911b07aff8cc4260303bfb3475b6024b3e653178
yaroslav <netpywka14@gmail.com>
2021-09-04 20:42:01 +0300

    statrt work with admin functions

---
+++ ElectronicStore/ElectronicStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,123 @@
+﻿using AutoMapper;
+using ElectronicStore.Core;
+using ElectronicStore.Core.Repositories;
+using ElectronicStore.Entities.Models;
+using ElectronicStore.WebUI.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectronicStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IClientRepo clientRepo = new ClientRepo();
+        private IProductRepo productRepo = new ProductRepo();
+        private ISaleRepo _saleRepo = new SaleRepo();
+        private ISellerRepo _sellerRepo = new SellerRepo();
+        
+        public AdminController()
+        {
+
+        }
+        // GET: Admin
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<PartialViewResult> LoadProducts(int? page)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductIndexView>());
+            var map = new Mapper(config);
+            var products = map.Map<List<ProductIndexView>>(await productRepo.GetItems());
+
+            int pageSize = 3;
+            int pageNumber = page ?? 1;
+
+            return PartialView(products.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Edit/5
+        public async Task<PartialViewResult> EditProduct(int id)
+        {
+            try
+            {
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductEditView>());
+                var map = new Mapper(config);
+                var product = map.Map<Product, ProductEditView>(await productRepo.GetById(id));
+
+                return PartialView(product);
+            }
+            catch
+            {
+                return PartialView("~/Views/Shared/Error.cshtml");
+            }
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
