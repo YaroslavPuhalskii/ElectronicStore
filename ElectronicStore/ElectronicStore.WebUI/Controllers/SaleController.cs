7a18dd69c1d7df46d7a2786c6162665a0902cb0b
yaroslav <netpywka14@gmail.com>
2021-09-09 11:03:22 +0300

    flter and pagging

--- ElectronicStore/ElectronicStore.WebUI/Controllers/SaleController.cs
+++ ElectronicStore/ElectronicStore.WebUI/Controllers/SaleController.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using AutoMapper;
+using ElectronicStore.Core;
+using ElectronicStore.Entities.Models;
+using ElectronicStore.WebUI.Models;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +14,53 @@ namespace ElectronicStore.WebUI.Controllers
 {
     public class SaleController : Controller
     {
-        // GET: Sale
-        public ActionResult Index()
+        private readonly ISaleRepo repo;
+
+        public SaleController(ISaleRepo saleRepo)
+        {
+            repo = saleRepo;
+        }
+
+        public ViewResult Index()
         {
             return View();
+        }
+
+        public async Task<PartialViewResult> Load(int? page, SaleFilter filter)
+        {
+            var items = await repo.GetItems();
+
+            if (filter.ClientFirstName != null)
+                items = items.Where(x => x.Client.FirstName == filter.ClientFirstName);
+
+            if (filter.ClientLastName != null)
+                items = items.Where(x => x.Client.LastName == filter.ClientLastName);
+
+            if (filter.Product != null)
+                items = items.Where(x => x.Product.Name == filter.Product);
+
+            if (filter.Seller != null)
+                items = items.Where(x => x.Seller.Name == filter.Seller);
+
+            if (filter.Price > 0)
+                items = items.Where(x => x.Price == filter.Price);
+
+            if (filter.Date > 2000 && filter.Date < DateTime.Now.Year)
+                items = items.Where(x => x.SaleDate.Year == filter.Date);
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Sale, SaleIndexView>()
+            .ForMember("Client", opt => opt.MapFrom(c => c.Client.FirstName))
+            .ForMember("Product", opt => opt.MapFrom(p => p.Product.Name))
+            .ForMember("Seller", opt => opt.MapFrom(s => s.Seller.Name)));
+            var map = new Mapper(config);
+            var sales = map.Map<IEnumerable<Sale>, IEnumerable<SaleIndexView>>(items);
+
+            ViewBag.Filter = new SaleFilter();
+
+            int pageSize = 3;
+            int pageNumber = page ?? 1;
+
+            return PartialView(sales.ToPagedList(pageNumber, pageSize));
         }
     }
 }
---
+++ ElectronicStore/ElectronicStore.WebUI/Models/SaleViewModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectronicStore.WebUI.Models
+{
+    public class SaleFilter
+    {
+        public string ClientFirstName { get; set; }
+        public string ClientLastName { get; set; }
+        public string Product { get; set; }
+        public string Seller { get; set; }
+        public decimal Price { get; set; }
+        public int Date { get; set; }
+    }
+
+    public class SaleIndexView
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int SaleId { get; set; }
+        [Required]
+        [Display(Name = "Клиент")]
+        public string Client { get; set; }
+        [Required]
+        [Display(Name = "Продукт")]
+        public string Product { get; set; }
+        [Required]
+        [Display(Name = "Продавец")]
+        public string Seller { get; set; }
+        [Required]
+        [Display(Name = "Дата покупки")]
+        public DateTime SaleDate { get; set; }
+        [Required]
+        [Display(Name = "Стоимость")]
+        public decimal Price { get; set; }
+    }
+}
